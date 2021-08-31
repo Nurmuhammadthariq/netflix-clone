43323dcc6853d80d44c54fde58145805818afca6
thoriqnoer <thoriq.noer@gmail.com>
2021-09-01 05:56:13 +0700

    First Commit project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDftQnaLbYBFJCtXm_bEj4L6iJqf3Dew9c',
+  authDomain: 'netflix-clone-95483.firebaseapp.com',
+  projectId: 'netflix-clone-95483',
+  storageBucket: 'netflix-clone-95483.appspot.com',
+  messagingSenderId: '1093726849907',
+  appId: '1:1093726849907:web:f6ec2248f0848c5c985306',
+};
