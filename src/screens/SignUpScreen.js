72605ae4cf86ee3afcb3a16d58e2cbf6db22b0bf
thoriqnoer <thoriq.noer@gmail.com>
2021-09-04 13:46:05 +0700

    add signup screen

---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SignUpScreen.css';
+
+function SignUpScreen() {
+  return <div className="signupScreen">This is Sign in panel</div>;
+}
+
+export default SignUpScreen;
