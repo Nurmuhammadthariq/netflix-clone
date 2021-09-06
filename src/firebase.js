600f54b8bcb5a2e52f238305394bd4c1ec5db8e1
thoriqnoer <thoriq.noer@gmail.com>
2021-09-06 07:06:39 +0700

    connect app to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyDftQnaLbYBFJCtXm_bEj4L6iJqf3Dew9c',
   authDomain: 'netflix-clone-95483.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '1093726849907',
   appId: '1:1093726849907:web:f6ec2248f0848c5c985306',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
