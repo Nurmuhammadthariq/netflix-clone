0f065a707b1e5b5031bc8c641afd6cffe60cde96
thoriqnoer <thoriq.noer@gmail.com>
2021-09-06 07:05:54 +0700

    Manage user using redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        // Logged Out
+        dispatch(logout);
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
 
   return (
     <div className="app">
