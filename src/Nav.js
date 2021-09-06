294447f608c06f306ba2600a1c311c59378392b0
thoriqnoer <thoriq.noer@gmail.com>
2021-09-06 07:07:12 +0700

    Change url logo

--- src/Nav.js
+++ src/Nav.js
@@ -22,7 +22,7 @@ function Nav() {
       <div className="nav__contents">
         <img
           className="nav__logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png"
           alt=""
         />
 
